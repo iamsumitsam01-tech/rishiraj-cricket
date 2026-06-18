ee7a73197ec294357d5320a56f94b0c4d1c7c058
infinity_project <iamsumitsam01@gmail.com>
2026-06-18 15:44:53 +0530

    opposite

--- cricket_agency/settings.py
+++ cricket_agency/settings.py
@@ -174,7 +174,7 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 STORAGES = {
     "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
