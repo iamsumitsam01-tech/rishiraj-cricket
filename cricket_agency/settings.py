4d5dfe0199fe22d565eb16d48d73784777e55268
infinity_project <iamsumitsam01@gmail.com>
2026-06-18 14:35:28 +0530

    updates

--- cricket_agency/settings.py
+++ cricket_agency/settings.py
@@ -172,14 +172,14 @@ CLOUDINARY_STORAGE = {
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 STORAGES = {
     "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedtStaticFilesStorage",
     },
 }
 WHITENOISE_MANIFEST_STRICT = False
